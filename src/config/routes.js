b6992180f48fa9bc7fe24c05d2d4b42d0e91d9a5
Nguyen Phu Cuong <91473760+LittleCuong@users.noreply.github.com>
2022-09-26 09:11:53 +0700

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
 
 const routes = {
-    home: '/',
-    type: '/:type/',
-    details: '/:category/:id/',
-    register: '/register',
-    login: '/login',
-    private: '/user',
+    home: 'cng-movie/',
+    type: 'cng-movie/:type/',
+    details: 'cng-movie/:category/:id/',
+    register: 'cng-movie/register',
+    login: 'cng-movie/login',
+    private: 'cng-movie/user',
 };
 
 export default routes;
